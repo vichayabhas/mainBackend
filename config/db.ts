75aec0f0572ab037e4f150c06fb6f92f13dbc543
vichayabhas <6633227421@student.chula.ac.th>
2024-05-11 16:07:32 +0700

    Update db.ts

--- config/db.ts
+++ config/db.ts
@@ -1,7 +1,4 @@
 import mongoose from "mongoose";
-
-
-
 export default async function connectDB(){
     const conn = await mongoose.connect(process.env.MONGO_URI||'');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
